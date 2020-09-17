34a6833fc8d9670dcbc0ac1859aa6805de9ecbc4
Rafael Brito <rafaelbritohs@gmail.com>
2020-09-16 23:35:00 -0300

    Meu repositório sem adições

--- 02 - INTRODUÇÃO AOS ALGORITMOS E PROGRAMAÇÃO/2.2_Comandos_de_saida/index.php
+++ 02 - INTRODUÇÃO AOS ALGORITMOS E PROGRAMAÇÃO/2.2_Comandos_de_saida/index.php
@@ -1,17 +1,14 @@
 <?php
 require __DIR__ . '/../framework/config.php';
-PHPClassName('Aula 02 - Comandos de Saída');
 
 PHPClassSession('echo', __LINE__);
 
 echo "<p>Olá mundo</p>";
 echo 23 . PHP_EOL;
 
-$hello = "Olá mundo";
+$hello = "Olá Mundo!!";
 
 echo $hello;
-echo "<p> $hello </p>";
-echo "<p> {$hello} </p>";
 
 PHPClassSession('print_r', __LINE__);
 
@@ -20,7 +17,6 @@ $array = [
     "course" => "Programador WEB",
     "class" => "Comandos de Saída",
 ];
-
 echo "<pre>";
 print_r($array);
 echo "</pre>";
