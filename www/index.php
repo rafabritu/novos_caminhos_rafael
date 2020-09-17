9fe0f2bba439acd34e9b804fd1c1239df4e9fd86
Rafael Brito <rafaelbritohs@gmail.com>
2020-09-16 21:55:58 -0300

    Criando index.php

---
+++ www/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+    <title>Testando</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+    <?php
+
+    $start = "Vamos INICIAR";
+    echo "<h1>Olá Mundo!  {$start}</h1>";
+    echo "<p id='js'>Loading ... </p>"
+    ?>
+
+    <script src="assets/scripts.js"></script>
+</body>
+
+</html>
