63a798694b016408398c83ed510902dcc05a0a5b
Rafael Brito <rafaelbritohs@gmail.com>
2020-10-14 19:19:15 -0300

    Inicio_HTML

---
+++ 03 - WEBDESIGN/04_Tags_de_Conteudo/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> <!-- DOCUMENTO DO HTML 5 -->
+<html lang='pt-br'>
+<!-- IDIOMA DO SITE -->
+
+<head>
+    <meta charset='UTF-8'> <!-- CONJUNTO DE CARACTERES -->
+    <!-- VIEWPORT - ADAPTAÇÃO PARA MOBILE -->
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <!-- ROBOTS BUSCADORES DE INTERNET, PARA INDEXAÇÃO NOS BUSCADORES -->
+    <meta name='robots' content='index, follow'>
+    <!--Para não indexar use content='noindex, nofollow'> -->
+    <link rel='shortcut icon' href=''>
+    <!-- Ícone da aba -->
+
+    <!--CSS-->
+    <link rel='stylesheet' href=''>
+    <!--TÍTULO DA PÁGINA-->
+    <title>APRENDENDO HTML</title>
+</head>
+
+<!--CONTEÚDO EXIBIDO PARA O USUÁRIO-->
+
+<body>
+    <!--LINK PARA JAVA SCRIPT, COLOCAR NO FINAL, a excessão é para algo bem específico-->
+    <script> </script>
+</body>
+
+</html>
---
+++ www/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name='robots' content='index, follow'>
+    <link rel='shortcut icon' href=''>
+    <link rel='stylesheet' href=''>
+    <title>APRENDENDO HTML</title>
+</head>
+
+<body>
+    <h1>França quer imposto para Facebook, Apple, Google e Amazon ainda este ano</h1>
+    <h2>Sem avanços em negociação com a OCDE, França deve retomar cobrança de seu próprio imposto digital sobre gigantes
+        de tecnologia</h2>
+    <p>A França criou em 2019 uma lei para cobrar impostos de empresas digitais como Facebook, Apple, Google e Amazon. A
+        aplicação da taxa foi suspensa por conta de um acordo com outros países, que, em janeiro de 2020, se
+        comprometeram a lançar uma iniciativa conjunta. Insatisfeitos com a falta de avanços, os franceses anunciaram
+        que passarão a cobrar seu imposto já em dezembro.</p>
+    <p>A declaração foi feita nesta quarta-feira (14) pelo ministro das Finanças da França, Bruno Le Maire, que
+        considera uma atuação em conjunto da Europa como ideal. "Ou se aceita a prorrogação por meses, talvez anos, ou
+        se considera que impostos justos sobre atividades digitais são urgentes e, neste caso, a Europa dá o exemplo",
+        afirmou, segundo a Reuters. "Consideramos indispensável que a Europa dê o exemplo e adote a tributação digital o
+        mais rápido possível".</p>
+    <p>Inicialmente, a ideia era criar no segundo semestre de 2020 um imposto único entre os países da OCDE (Organização
+        para a Cooperação e Desenvolvimento Econômico). Nesta semana, no entanto, a entidade anunciou que a proposta não
+        será apresentada no prazo previsto, devendo ficar somente para 2021.</p>
+    <p>Com a cobrança, companhias como Facebook, Apple, Google e Amazon passariam a ser taxadas nos locais em que
+        prestam serviços — hoje a cobrança só acontece onde estão sediadas. Prevendo queda na arrecadação, os Estados
+        Unidos se tornaram os principais críticos da medida e ameaçaram iniciar uma guerra comercial, com aumento de
+        impostos sobre produtos de luxo feitos na França.</p>
+    <h3>União Europeia pode ter imposto digital</h3>
+    <ul>
+        <li>A criação do imposto digital</li>
+        <li>também tem o apoio de Espanha e Itália</li>
+        <ol>
+            <li>além da Comissão Europeia.</li>
+        </ol>
+        <li>Isso pode fazer com que a União Europeia</li>
+        <li>passe à frente da OCDE e volte a discutir</li>
+        <li>sua própria proposta na primeira metade de 2021</li>
+    </ul>
+    <!--<ol reversed> -->
+    <!--<ol start= '3'> -->
+    <!--<ol type'I'> -->
+    <!--<ol type='A'> -->
+    <ol type='a'>
+        <li>Ainda em 2020, o bloco também deve apresentar </li>
+        <ul>
+            <li>propostas para regular gigantes de tecnologia </li>
+        </ul>
+        <li>evitar práticas como o favorecimento de apps</li>
+        <li>das empresas em seus próprios sistemas operacionais.</li>
+    </ol>
+
+    <a href="http://www.megabyteinfor.site" target="blank" title="Informática" alt="Informática">Megabyte</a>
+    <script> </script>
+</body>
+
+</html>
