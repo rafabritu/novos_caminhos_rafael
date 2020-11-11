0fe142b7f7b98312e3c06f0f40d42b40bdade5f4
Rafael Brito <rafaelbritohs@gmail.com>
2020-11-10 23:15:44 -0300

    Aula 19.3

--- www/Testes/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/../framework/config.php';
-PHPClassName('TESTES');
-
-/*
-*
-*/
-PHPClassSession('', __LINE__);
--- www/index.php
+++ www/index.php
@@ -13,11 +13,66 @@
 <body>
     <header>
         <h1>
-            <p ID="titulo">
-                Exemplo de seletores
+            <p>
+                Seja bem vindo ao meu site de seletores
             </p>
         </h1>
+        <p>Exemplo de seletores</p>
     </header>
+    <main>
+        <a href="#" class="btn btn_green">Botão</a>
+        <a href="#" class="btn btn_red">Excluir</a>
+        <section class="main_blog radius">
+            <header>
+                <h2>Sessão Principal de Conteúdo</h2>
+            </header>
+            <article class="radius">
+                <header>
+                    <h3 class="title_medium_lightray">Teste de Artigo</h3>
+                </header>
+                <p>
+                    At ratione molestiae qui neque quis. Ea omnis eligendi qui voluptas eos est nulla. Fuga accusamus
+                    mollitia quam voluptas doloremque expedita repellat.
+                </p>
+            </article>
+            <article class="radius">
+                <header>
+                    <h3></h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    At ratione molestiae qui neque quis. Ea omnis eligendi qui voluptas eos est nulla. Fuga accusamus
+                    mollitia quam voluptas doloremque expedita repellat.
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    At ratione molestiae qui neque quis. Ea omnis eligendi qui voluptas eos est nulla. Fuga accusamus
+                    mollitia quam voluptas doloremque expedita repellat.
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    At ratione molestiae qui neque quis. Ea omnis eligendi qui voluptas eos est nulla. Fuga accusamus
+                    mollitia quam voluptas doloremque expedita repellat.
+                </p>
+            </article>
+        </section>
+    </main>
+    <article>
+        <header>
+            <h3>Teste de Artigo</h3>
+        </header>
+        <p>
+            At ratione molestiae qui neque quis. Ea omnis eligendi qui voluptas eos est nulla. Fuga accusamus
+            mollitia quam voluptas doloremque expedita repellat.
+        </p>
+    </article>
     <p>
         Isso aqui é um bloco
     </p>
